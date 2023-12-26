f13904d31726b36136f76ceb64080124704aa199
Rioba-Ian <riobafelix@gmail.com>
2023-12-26 16:12:35 +0300

    design navbar and mobile navbar plus functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {createSignal} from "solid-js";
+import {Navbar} from "./components/Navbar";
 
 function App() {
  const [count, setCount] = createSignal(0);
 
  return (
-  <>
-   <h1>Hello Mum.</h1>
-  </>
+  <div style={{width: "min(80%, 1440px)", margin: "0 auto"}}>
+   <Navbar />
+  </div>
  );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import {render} from "solid-js/web";
+import {Router, Route} from "@solidjs/router";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+ () => (
+  <Router>
+   <Route path="/" component={App} />
+  </Router>
+ ),
+ root!
+);
