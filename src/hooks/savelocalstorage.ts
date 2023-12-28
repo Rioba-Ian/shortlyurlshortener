439eb389e4d63002b6b21370862ff15ad8353f3a
Rioba-Ian <riobafelix@gmail.com>
2023-12-28 18:37:16 +0300

    deal with status for showing the links using localstorage

---
+++ src/hooks/savelocalstorage.ts
@@ -0,0 +1,21 @@
+import {Signal, createEffect, createSignal} from "solid-js";
+
+function useLocalStorage<T>(
+ key: string,
+ defaultValue: T,
+ storage = localStorage
+): Signal<T> {
+ const item = storage.getItem(key);
+ const initialValue = item ? (JSON.parse(item) as T) : defaultValue;
+ const [value, setValue] = createSignal<T>(initialValue);
+
+ const setValueAndStore = ((arg) => {
+  const v = setValue(arg);
+  storage.setItem(key, JSON.stringify(v));
+  return v;
+ }) as typeof setValue;
+
+ return [value, setValueAndStore];
+}
+
+export default useLocalStorage;
